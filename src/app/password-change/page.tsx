56930b9f3c26665664983624879fcfc2fca9ca0b
maksim4k1 <maksim4k1@gmail.com>
2024-02-20 17:39:54 +0700

    created password change page component and added markup for password change form

---
+++ src/app/password-change/page.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import Form from "@/components/UI/Form";
+import Input from "@/components/UI/Input";
+import Button from "@/components/UI/Button";
+import { ChangeEvent, FormEvent, FunctionComponent, useState } from "react";
+
+interface Props {}
+
+const initialState = {
+  currentPassword: "",
+  newPassword: "",
+  confirmPassword: "",
+};
+
+const PasswordChangePage: FunctionComponent<Props> = ({}) => {
+  const [formState, setFormState] = useState(initialState);
+
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>): void => {
+    setFormState((currentFormState) => ({
+      ...currentFormState,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const onSubmitHandler = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    setFormState(initialState);
+    console.log(formState);
+  };
+
+  return (
+    <div>
+      <Form onSubmit={onSubmitHandler}>
+        <div>Смена пароля</div>
+        <Input
+          name="currentPassword"
+          type="text"
+          placeholder="Старый пароль"
+          onChange={onChangeHandler}
+        />
+        <Input
+          name="newPassword"
+          type="password"
+          placeholder="Новый пароль"
+          onChange={onChangeHandler}
+        />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Подтвердите пароль"
+          onChange={onChangeHandler}
+        />
+        <Button>Продолжить</Button>
+      </Form>
+    </div>
+  );
+};
+
+export default PasswordChangePage;
