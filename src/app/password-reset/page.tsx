e2906d537ceacdacadc5587cce4022f451d00a1a
maksim4k1 <maksim4k1@gmail.com>
2024-02-20 17:39:26 +0700

    created password reset page component and added markup for password reset form

---
+++ src/app/password-reset/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Form from "@/components/UI/Form";
+import Input from "@/components/UI/Input";
+import Button from "@/components/UI/Button";
+import { ChangeEvent, FormEvent, FunctionComponent, useState } from "react";
+
+interface Props {}
+
+const PasswordResetPage: FunctionComponent<Props> = ({}) => {
+  const [email, setEmail] = useState("");
+
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>): void => {
+    setEmail(event.target.value);
+  };
+
+  const onSubmitHandler = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    setEmail("");
+    console.log(email);
+  };
+
+  return (
+    <div>
+      <Form onSubmit={onSubmitHandler}>
+        <div>Восстановление пароля</div>
+        <Input
+          name="email"
+          type="email"
+          placeholder="Введите email"
+          onChange={onChangeHandler}
+        />
+        <Button>Продолжить</Button>
+      </Form>
+    </div>
+  );
+};
+
+export default PasswordResetPage;
