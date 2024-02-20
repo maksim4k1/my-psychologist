a8da2595a70134cf54a218c59aca1ab0337c0662
maksim4k1 <maksim4k1@gmail.com>
2024-02-20 16:49:17 +0700

    created button UI component

---
+++ src/components/UI/Button/index.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { FunctionComponent, ReactNode } from "react";
+import styles from "./styles.module.scss";
+
+interface Props {
+  children: ReactNode;
+  className?: string;
+  [key: string]: any;
+}
+
+const Component: FunctionComponent<Props> = ({
+  children,
+  className = "",
+  ...props
+}) => {
+  return (
+    <button
+      className={`${styles.button} ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Component;
