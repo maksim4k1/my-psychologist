fc52c5292e4b710b8b5c24119394b47c668fe274
maksim4k1 <maksim4k1@gmail.com>
2024-02-22 10:14:49 +0700

    edited currentPassword input rendering logic on password-change page

--- src/app/password-change/page.tsx
+++ src/app/password-change/page.tsx
@@ -37,7 +37,7 @@ const PasswordChangePage: FunctionComponent<Props> = ({}) => {
     <div>
       <Form onSubmit={onSubmitHandler}>
         <div>Смена пароля</div>
-        {!!resetConfirmationCode && (
+        {!resetConfirmationCode && (
           <Input
             name="currentPassword"
             type="text"
