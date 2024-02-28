68fe96dfe288332ad81dbc5022b39f082cbc7f12
maksim4k1 <maksim4k1@gmail.com>
2024-02-28 21:05:16 +0700

    created PageTitle component

---
+++ src/components/UI/Titles/PageTitle/index.tsx
@@ -0,0 +1,25 @@
+import { FunctionComponent } from "react";
+import styles from "./styles.module.scss";
+
+interface Props {
+  children: string;
+  className?: string;
+  [key: string]: any;
+}
+
+const PageTitle: FunctionComponent<Props> = ({
+  children,
+  className,
+  ...props
+}) => {
+  return (
+    <h1
+      className={`${styles.title} ${className}`}
+      {...props}
+    >
+      {children}
+    </h1>
+  );
+};
+
+export default PageTitle;
