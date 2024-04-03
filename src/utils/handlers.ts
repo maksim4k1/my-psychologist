8fa62afc1d8df6af75e44f261087d2fdb1a59e48
maksim4k1 <maksim4k1@gmail.com>
2024-04-03 18:05:56 +0700

    added type for checkboxes data in handlers.ts file

--- src/utils/handlers.ts
+++ src/utils/handlers.ts
@@ -1,32 +1,37 @@
 import { ChangeEvent } from "react";
 
-export const onChangeInputHandler = (setFormData: CallableFunction) => (event: ChangeEvent<HTMLInputElement>) => {
-  let type: string = event.target.type;
-  let name: string = event.target.name;
-  let value: string = event.target.value;
+export const onChangeInputHandler =
+  (setFormData: CallableFunction) => (event: ChangeEvent<HTMLInputElement>) => {
+    interface CheckboxData {
+      [key: string]: string;
+    }
 
-  if (type === "radio") {
-    setFormData((data: Object) => ({
-      ...data,
-      [event.target.name]: event.target.value,
-    }));
-  } else if (type === "checkbox") {
-    setFormData((data: Object) => {
-      if (!Object.keys(data).includes(name)) return data;
-      let set: Set<string> = new Set(data[name]);
+    let type: string = event.target.type;
+    let name: string = event.target.name;
+    let value: string = event.target.value;
 
-      if (set.has(value)) set.delete(value);
-      else set.add(value);
-
-      return {
+    if (type === "radio") {
+      setFormData((data: Object) => ({
         ...data,
-        [name]: Array.from(set),
-      };
-    });
-  } else{
-    setFormData((data: Object) => ({
-      ...data,
-      [event.target.name]: event.target.value,
-    }));
-  }
-}
+        [event.target.name]: event.target.value,
+      }));
+    } else if (type === "checkbox") {
+      setFormData((data: CheckboxData) => {
+        if (!Object.keys(data).includes(name)) return data;
+        let set: Set<string> = new Set(data[name]);
+
+        if (set.has(value)) set.delete(value);
+        else set.add(value);
+
+        return {
+          ...data,
+          [name]: Array.from(set),
+        };
+      });
+    } else {
+      setFormData((data: Object) => ({
+        ...data,
+        [event.target.name]: event.target.value,
+      }));
+    }
+  };
