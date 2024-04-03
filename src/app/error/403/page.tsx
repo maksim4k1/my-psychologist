952a01fa68a871d08f12c92f7b5ef8f507773f79
maksim4k1 <maksim4k1@gmail.com>
2024-04-04 00:02:07 +0700

    created 404 and 403 error pages

---
+++ src/app/error/403/page.tsx
@@ -0,0 +1,12 @@
+import PageTitle from "@/components/UI/Titles/PageTitle";
+import Container from "@/components/UI/Container";
+
+function Error403Page() {
+  return (
+    <Container>
+      <PageTitle>403 - отказано в доступе</PageTitle>
+    </Container>
+  );
+}
+
+export default Error403Page;
---
+++ src/app/error/404/page.tsx
@@ -0,0 +1,12 @@
+import PageTitle from "@/components/UI/Titles/PageTitle";
+import Container from "@/components/UI/Container";
+
+function Error404Page() {
+  return (
+    <Container>
+      <PageTitle>404 - страница не найдена</PageTitle>
+    </Container>
+  );
+}
+
+export default Error404Page;
