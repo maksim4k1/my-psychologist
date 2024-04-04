8e3b8ab465b16b0a6b29806d3f8b0c2b0e01d785
maksim4k1 <maksim4k1@gmail.com>
2024-04-04 17:45:13 +0700

    created pages for different errors authorization related

---
+++ src/components/errors/OnlyForPsychologist.tsx
@@ -0,0 +1,12 @@
+import PageTitle from "@/components/UI/Titles/PageTitle";
+import Container from "@/components/UI/Container";
+
+function OnlyForPsychologistErrorPage() {
+  return (
+    <Container>
+      <PageTitle>Данная страница доступна только психологу</PageTitle>
+    </Container>
+  );
+}
+
+export default OnlyForPsychologistErrorPage;
