ef6fedcd56d5ffeccbf0be26a6f7a023bba27674
maksim4k1 <maksim4k1@gmail.com>
2024-04-04 17:47:22 +0700

    created checkAuth hoc to check access

---
+++ src/components/hocs/checkAuth.tsx
@@ -0,0 +1,44 @@
+import { ACCESS } from "../../../config/access.config";
+import { FunctionComponent } from "react";
+import NeedAuthErrorPage from "../errors/NeedAuth";
+import OnlyForPsychologistErrorPage from "../errors/OnlyForPsychologist";
+import AccessDeniedErrorPage from "../errors/AccessDenied";
+import OnlyForUnauthorizedErrorPage from "../errors/OnlyForUnautorized";
+
+function checkAuth(
+  Component: FunctionComponent,
+  isNeedAuth: boolean = false,
+  accessFor: string[] = [ACCESS.public],
+): FunctionComponent {
+  return () => {
+    const isAuth: boolean = true;
+    const currentRole: string = !isAuth
+      ? ACCESS.unauthorized
+      : ACCESS.psychologist;
+
+    if (isNeedAuth && !isAuth) {
+      return <NeedAuthErrorPage />;
+    } else if (
+      !isNeedAuth &&
+      isAuth &&
+      accessFor.includes(ACCESS.unauthorized) &&
+      accessFor.length === 1
+    ) {
+      return <OnlyForUnauthorizedErrorPage />;
+    }
+
+    if (
+      accessFor.includes(ACCESS.public) ||
+      accessFor.includes(currentRole) ||
+      (isAuth && accessFor.includes(ACCESS.client))
+    ) {
+      return <Component />;
+    } else if (isAuth && accessFor.includes(ACCESS.psychologist)) {
+      return <OnlyForPsychologistErrorPage />;
+    } else {
+      return <AccessDeniedErrorPage />;
+    }
+  };
+}
+
+export default checkAuth;
