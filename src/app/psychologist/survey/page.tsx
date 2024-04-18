77dca6565b3ab48c5604318cf41e76ea14ad6b63
maksim4k1 <maksim4k1@gmail.com>
2024-04-18 20:20:39 +0700

    deleted unnecessary elements in PsychologistSurvey page

--- src/app/psychologist/survey/page.tsx
+++ src/app/psychologist/survey/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 
 import PageTitle from "@/components/UI/Titles/PageTitle";
-import Radio from "@/components/UI/Inputs/Radio";
 import styles from "./styles.module.scss";
 import Container from "@/components/UI/Container";
-import Checkbox from "@/components/UI/Inputs/Checkbox";
 import Form from "@/components/UI/Forms/Form";
-import Subtitle from "@/components/UI/Titles/Subtitle";
 import Input from "@/components/UI/Inputs/Input";
 import PrimaryButton from "@/components/UI/Buttons/PrimaryButton";
 import checkAuth from "@/components/hocs/checkAuth";
@@ -50,131 +47,59 @@ function PsychologistSurveyPage() {
         className={styles.form}
         onSubmit={onSubmitHandler}
       >
-        <div className={styles.leftInputs}>
-          <Subtitle className={styles.subtitle}>Общая информация</Subtitle>
-          <div className={styles.inputs}>
-            <Input
-              name="fullName"
-              type="text"
-              placeholder="ФИО"
-              value={fullName.value}
-              onChange={fullName.onChange}
-            />
-            <MaskedInput
-              name="birthday"
-              type="text"
-              placeholder="Дата рождения  (ДД.ММ.ГГГГ)"
-              value={birthday.value}
-              mask={Date}
-              max={new Date()}
-              onBlur={birthday.onBlur}
-              onAccept={birthday.onAccept}
-              errorText={birthday.error}
-            />
-            <Input
-              name="education"
-              type="text"
-              placeholder="Образование"
-              value={education.value}
-              onChange={education.onChange}
-            />
-            <Input
-              name="about"
-              type="text"
-              placeholder="О себе"
-              value={about.value}
-              onChange={about.onChange}
-            />
-            <Input
-              name="city"
-              type="text"
-              placeholder="Город"
-              value={city.value}
-              onChange={city.onChange}
-            />
-            <Input
-              name="workFormat"
-              type="text"
-              placeholder="Формат работы (очно, дистанционно и тд.)"
-              value={workFormat.value}
-              onChange={workFormat.onChange}
-            />
-          </div>
-          <PrimaryButton
-            type="submit"
-            className={styles.button}
-            isMedium={true}
-          >
-            Сохранить
-          </PrimaryButton>
-        </div>
-        <div className={styles.rightInputs}>
-          <div>
-            <Subtitle className={styles.subtitle}>Пол</Subtitle>
-            <div className={styles.radios}>
-              <Radio
-                name="gender"
-                value="man"
-                defaultChecked
-                labelText="Мужской"
-                onChange={gender.onChange}
-              />
-              <Radio
-                name="gender"
-                value="woman"
-                labelText="Женский"
-                onChange={gender.onChange}
-              />
-              <Radio
-                name="gender"
-                value="other"
-                labelText="Другой"
-                onChange={gender.onChange}
-              />
-            </div>
-          </div>
-          <div>
-            <Subtitle className={styles.subtitle}>Специализация</Subtitle>
-            <div className={styles.checkboxes}>
-              <Checkbox
-                name="specialization"
-                value="Депрессия"
-                labelText="Депрессия"
-                onChange={specialization.onChange}
-              />
-              <Checkbox
-                name="specialization"
-                value="Тревога"
-                labelText="Тревога"
-                onChange={specialization.onChange}
-              />
-              <Checkbox
-                name="specialization"
-                value="ПТСР"
-                labelText="ПТСР"
-                onChange={specialization.onChange}
-              />
-              <Checkbox
-                name="specialization"
-                value="Растройство пищевого поведения"
-                labelText="Растройство пищевого поведения"
-                onChange={specialization.onChange}
-              />
-              <Checkbox
-                name="specialization"
-                value="Зависимости"
-                labelText="Зависимости"
-                onChange={specialization.onChange}
-              />
-              <Checkbox
-                name="specialization"
-                value="Другое"
-                labelText="Другое"
-                onChange={specialization.onChange}
-              />
-            </div>
-          </div>
-        </div>
+        <Input
+          name="fullName"
+          type="text"
+          placeholder="ФИО"
+          value={fullName.value}
+          onChange={fullName.onChange}
+        />
+        <MaskedInput
+          name="birthday"
+          type="text"
+          placeholder="Дата рождения  (ДД.ММ.ГГГГ)"
+          value={birthday.value}
+          mask={Date}
+          max={new Date()}
+          onBlur={birthday.onBlur}
+          onAccept={birthday.onAccept}
+          errorText={birthday.error}
+        />
+        <Input
+          name="education"
+          type="text"
+          placeholder="Образование"
+          value={education.value}
+          onChange={education.onChange}
+        />
+        <Input
+          name="about"
+          type="text"
+          placeholder="О себе"
+          value={about.value}
+          onChange={about.onChange}
+        />
+        <Input
+          name="city"
+          type="text"
+          placeholder="Город"
+          value={city.value}
+          onChange={city.onChange}
+        />
+        <Input
+          name="workFormat"
+          type="text"
+          placeholder="Формат работы (очно, дистанционно и тд.)"
+          value={workFormat.value}
+          onChange={workFormat.onChange}
+        />
+        <PrimaryButton
+          type="submit"
+          className={styles.button}
+          isMedium={true}
+        >
+          Сохранить
+        </PrimaryButton>
       </Form>
     </Container>
   );
