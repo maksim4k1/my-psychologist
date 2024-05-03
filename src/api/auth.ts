00b3d4a71576f8f46c05c82fdfbe617a1013b1fc
maksim4k1 <maksim4k1@gmail.com>
2024-05-03 20:47:58 +0700

    connected auth requests to api

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,14 +1,29 @@
 import { LoginPayload, RegisterPayload } from "@/redux/features/auth/types";
 import { AppDispatch } from "./../redux/store";
 import { authActions } from "@/redux/features/auth/";
+import { API } from "../../config/api.config";
+import { deleteToken, saveToken } from "@/storage/token";
 
 export const login: Function =
   (formData: LoginPayload) => async (dispatch: AppDispatch) => {
     dispatch(authActions.loginLoading());
     try {
-      console.log(formData);
+      const response = await fetch(`${API}/users/auth`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
 
-      dispatch(authActions.loginSuccess());
+      const data = await response.json();
+
+      if (typeof data === "string") {
+        dispatch(authActions.loginError(data));
+      } else {
+        dispatch(authActions.loginSuccess(data));
+        saveToken(data.token);
+      }
     } catch (err) {
       dispatch(
         authActions.loginError(
@@ -21,15 +36,61 @@ export const login: Function =
 export const register: Function =
   (formData: RegisterPayload) => async (dispatch: AppDispatch) => {
     dispatch(authActions.registerLoading());
-    try {
-      console.log(formData);
 
-      dispatch(authActions.registerSuccess());
+    try {
+      const response = await fetch(`${API}/users/reg`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: formData.email,
+          username: formData.name,
+          password: formData.password,
+          confirm_password: formData.confirmPassword,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (typeof data === "string") {
+        dispatch(authActions.registerError(data));
+      } else {
+        dispatch(authActions.registerSuccess(data));
+      }
     } catch (err) {
       dispatch(
         authActions.registerError(
           err instanceof Error ? err.message : String(err),
         ),
       );
+    }
+  };
+
+export const loginByToken: Function =
+  (token: string) => async (dispatch: AppDispatch) => {
+    dispatch(authActions.loginLoading());
+    try {
+      const response = await fetch(`${API}/users/auth_token`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token: token,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (typeof data === "string") {
+        deleteToken();
+        dispatch(authActions.loginError());
+      } else {
+        dispatch(authActions.loginSuccess(data));
+      }
+    } catch (err) {
+      deleteToken();
+      dispatch(authActions.loginError());
     }
   };
