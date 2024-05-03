a108a91ff6b7381d5fd06089f3922e20c2039bf5
maksim4k1 <maksim4k1@gmail.com>
2024-05-03 21:46:01 +0700

    edited psychologist page structure

--- src/app/psychologist/page.tsx
+++ src/app/psychologist/page.tsx
@@ -1,48 +1,100 @@
 "use client";
 
-import GroupIcon from "@/assets/svg/Icons/GroupIcon";
 import MessageIcon from "@/assets/svg/Icons/MessageIcon";
 import Container from "@/components/UI/Container";
 import PageTitle from "@/components/UI/Titles/PageTitle";
 import IconTextLink from "@/components/UI/Links/IconTextLink";
 import styles from "./styles.module.scss";
-import { FunctionComponent } from "react";
-import ProfileImage from "@/components/UI/Images/ProfileImage";
-import PrimarySwiper from "@/components/UI/PrimarySwiper";
+import { ReactNode } from "react";
 import checkAuth from "@/components/hocs/checkAuth";
 import { ACCESS } from "../../../config/access.config";
+import ApplicationCard from "@/components/UI/Cards/ApplicationCard";
+import ClientCard from "@/components/UI/Cards/ClientCard";
 
-interface Props {
+interface Application {
+  userId: number;
+  profileImage: string;
   username: string;
-  content: string;
+  isOnline: boolean;
+  problem: string;
 }
 
-const ApplicationCard: FunctionComponent<Props> = ({ username, content }) => {
-  return (
-    <div className={styles.card}>
-      <ProfileImage
-        src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
-        alt="profile"
-        size={40}
-      />
-      <div className={styles.info}>
-        <div className={styles.username}>{username}</div>
-        <div className={styles.content}>{content}</div>
-      </div>
-    </div>
-  );
-};
+interface Client {
+  userId: number;
+  profileImage: string;
+  username: string;
+  isOnline: boolean;
+  problems: string[];
+}
 
 async function PsychologistPage() {
+  const applications: Application[] = [
+    {
+      userId: 1,
+      profileImage: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+      username: "Евгений",
+      isOnline: true,
+      problem:
+        "Здравствуйте! За эту неделю я выгорел уже 20 раз и больше не намерен это терпеть",
+    },
+    {
+      userId: 2,
+      profileImage: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+      username: "Евгений",
+      isOnline: true,
+      problem: "Я думаю, что за мной следит утка, что мне делать?",
+    },
+    {
+      userId: 3,
+      profileImage: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+      username: "Александра",
+      isOnline: true,
+      problem: "Привет, я не люблю общаться с людьми, только со своей собакой",
+    },
+    {
+      userId: 4,
+      profileImage: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+      username: "Ангелина",
+      isOnline: true,
+      problem:
+        "Здравствуйте! Я страдаю от паниеских атак почти три года и хотела бы сделать с этим что нибудь, вы можете мне помочь? ",
+    },
+  ];
+  const clients: Client[] = [
+    {
+      userId: 1,
+      profileImage: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+      username: "Александр",
+      isOnline: false,
+      problems: ["Депрессия", "Зависимости", "Выгорание"],
+    },
+    {
+      userId: 2,
+      profileImage: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+      username: "Алиса",
+      isOnline: true,
+      problems: ["ПТСР", "Тревога", "РПП", "Депрессия"],
+    },
+    {
+      userId: 3,
+      profileImage: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+      username: "Виктор",
+      isOnline: true,
+      problems: ["Выгорание"],
+    },
+    {
+      userId: 4,
+      profileImage: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+      username: "Ангелина",
+      isOnline: false,
+      problems: ["Панические атаки", "СДВГ", "Социофобия"],
+    },
+  ];
+
   return (
     <Container>
       <PageTitle>Кабинет психолога</PageTitle>
       <nav className={styles.navigation}>
-        <IconTextLink
-          href="/psychologist/clients"
-          icon={<GroupIcon />}
-          content="Клиенты"
-        />
         <IconTextLink
           href=""
           icon={<MessageIcon />}
@@ -53,24 +105,31 @@ async function PsychologistPage() {
 
       <div>
         <h2 className={styles.subtitle}>Заявки</h2>
-        <PrimarySwiper gap={8}>
-          <ApplicationCard
-            username="Евгений"
-            content="Здравствуйте! За эту неделю я выгорел уже 20 раз и больше не намерен это терпеть"
-          />
-          <ApplicationCard
-            username="Лаврентий"
-            content="Я думаю, что за мной следит утка, что мне делать?"
-          />
-          <ApplicationCard
-            username="Александра"
-            content="Привет, я не люблю общаться с людьми, только со своей собакой"
-          />
-          <ApplicationCard
-            username="Александра"
-            content="Привет, я не люблю общаться с людьми, только со своей собакой"
-          />
-        </PrimarySwiper>
+        <div className={styles.list}>
+          {applications.map((application) => {
+            return (
+              <ApplicationCard
+                key={application.userId}
+                client={application}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <div>
+        <h2 className={`${styles.subtitle} ${styles.clientsSubtitle}`}>
+          Мои клиенты
+        </h2>
+        <div className={styles.list}>
+          {clients.map((client) => {
+            return (
+              <ClientCard
+                key={client.userId}
+                client={client}
+              />
+            );
+          })}
+        </div>
       </div>
     </Container>
   );
