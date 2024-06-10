72207521b9728cd0b1e38546245a5c641a9f9545
maksim4k1 <maksim4k1@gmail.com>
2024-06-10 20:30:34 +0700

    created LoadingWrapper component

---
+++ src/components/wrappers/LoadingWrapper/index.tsx
@@ -0,0 +1,21 @@
+import LoadingLoop from "@/components/UI/LoadingLoop";
+import { FunctionComponent, ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  status: boolean | boolean[];
+}
+
+const LoadingWrapper: FunctionComponent<Props> = ({ children, status }) => {
+  let isLoading: boolean;
+
+  if (Array.isArray(status)) {
+    isLoading = status.reduce((acc, el) => acc || el, false);
+  } else {
+    isLoading = status;
+  }
+
+  return isLoading ? <LoadingLoop /> : children;
+};
+
+export default LoadingWrapper;
