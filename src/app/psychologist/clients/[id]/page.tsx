3e41ddcac396f0cdcb0712fa3682b8fc3fbbecd9
maksim4k1 <maksim4k1@gmail.com>
2024-06-13 20:12:33 +0700

    moved the components of the cards into separate files

--- src/app/psychologist/clients/[id]/page.tsx
+++ src/app/psychologist/clients/[id]/page.tsx
@@ -3,16 +3,11 @@
 import Container from "@/components/UI/Container";
 import PageTitle from "@/components/UI/Titles/PageTitle";
 import styles from "./styles.module.scss";
-import { FunctionComponent, useEffect } from "react";
-import ProfileImage from "@/components/UI/Images/ProfileImage";
-import Symptom from "@/components/UI/Symptom";
-import MoreVerticalIcon from "@/assets/svg/Icons/MoreVerticalIcon";
+import { useEffect } from "react";
 import PrimaryButton from "@/components/UI/Buttons/PrimaryButton";
 import Subtitle from "@/components/UI/Titles/Subtitle";
-import Link from "next/link";
 import checkAuth from "@/components/hocs/checkAuth";
 import { ACCESS } from "../../../../../config/access.config";
-import { ClientProfileData } from "@/redux/features/clients/types";
 import { useAppDispatch, useAppSelector } from "@/hooks/reduxHooks";
 import { useParams } from "next/navigation";
 import {
@@ -21,72 +16,8 @@ import {
 } from "@/redux/features/clients/selectors";
 import ClientsService from "@/api/clients";
 import LoadingWrapper from "@/components/wrappers/LoadingWrapper";
-
-interface ClientCardProps {
-  client: ClientProfileData;
-}
-
-const ClientCard: FunctionComponent<ClientCardProps> = ({ client }) => {
-  return (
-    <div className={styles.card}>
-      <div className={styles.cardHeader}>
-        <div className={styles.profileInfo}>
-          <div className={styles.username}>{client.username}</div>
-          <div
-            className={`${styles.status} ${
-              client.isOnline ? styles.isOnline : ""
-            }`}
-          >
-            {client.isOnline ? "Онлайн" : "Был(а) недавно"}
-          </div>
-        </div>
-        {/* <button className={styles.moreButton}>
-          <MoreVerticalIcon />
-        </button> */}
-      </div>
-      <ProfileImage
-        className={styles.profileImage}
-        size={188}
-        src={client.profileImage}
-        alt={client.username}
-      />
-      <div className={styles.info}>Возраст: {client.age} лет</div>
-      {!!client.problems.length && <div className={styles.info}>Запрос:</div>}
-      <div className={styles.problems}>
-        {client.problems.map((problem, index) => {
-          return (
-            <Symptom
-              key={index}
-              problem={problem}
-            />
-          );
-        })}
-      </div>
-      {/* <PrimaryButton className={styles.cardButton}>Чат</PrimaryButton> */}
-    </div>
-  );
-};
-
-interface TestProps {
-  test: {
-    id: number;
-    title: string;
-  };
-}
-
-const TestCard: FunctionComponent<TestProps> = ({ test }) => {
-  return (
-    <div className={styles.testCard}>
-      <div className={styles.testTitle}>{test.title}</div>
-      <Link
-        className={styles.testLink}
-        href={`/psychologist/clients/result/overall`}
-      >
-        Посмотреть результаты
-      </Link>
-    </div>
-  );
-};
+import TestCard from "@/components/UI/Cards/TestCard";
+import ClientProfileCard from "@/components/UI/Cards/ClientProfileCard";
 
 function PsychologistClientPage() {
   const { id } = useParams();
@@ -103,7 +34,7 @@ function PsychologistClientPage() {
       <PageTitle className={styles.title}>Профиль клиента</PageTitle>
       <div className={styles.main}>
         <LoadingWrapper status={clientState.isLoading}>
-          {client && <ClientCard client={client} />}
+          {client && <ClientProfileCard client={client} />}
           <div>
             <Subtitle>Пройденные тесты</Subtitle>
             <div className={styles.tests}>
