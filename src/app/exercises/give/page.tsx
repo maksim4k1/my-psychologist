33d05b4760853dc91b5b3f68b3ad3b8fde49f169
maksim4k1 <maksim4k1@gmail.com>
2024-06-20 12:13:17 +0700

    replaced local ExerciseCard component with UI ExerciseCard on GiveExercise page

--- src/app/exercises/give/page.tsx
+++ src/app/exercises/give/page.tsx
@@ -5,89 +5,44 @@ import Container from "@/components/UI/Container";
 import PageTitle from "@/components/UI/Titles/PageTitle";
 import styles from "./styles.module.scss";
 import Subtitle from "@/components/UI/Titles/Subtitle";
-import { FunctionComponent } from "react";
 import { ACCESS } from "../../../../config/access.config";
-
-interface Props {
-  title: string;
-  description: string;
-}
-
-const ExerciseCard: FunctionComponent<Props> = ({ title, description }) => {
-  return (
-    <div className={styles.card}>
-      <h5 className={styles.title}>{title}</h5>
-      <p className={styles.description}>{description}</p>
-    </div>
-  );
-};
-
-interface TaskInfo {
-  title: string;
-  description: string;
-}
+import { TestData } from "@/redux/features/tests/types";
+import { useSearchParams } from "next/navigation";
+import ExerciseCard from "@/components/UI/Cards/ExerciseCard";
 
 function GiveExercisesPage() {
-  const tests: TaskInfo[] = [
+  const searchParams = useSearchParams();
+
+  const tests: TestData[] = [
     {
+      id: "1",
       title: "Тест на депрессию",
       description:
         "Тест по методике знаменитого американского психотерапевта Аарона Бека",
     },
     {
+      id: "2",
       title: "Индикатор копинг-стратегий",
       description:
         "Предназначен для диагностики доминирующих копинг-стратегий личности",
     },
     {
+      id: "3",
       title: "Профессиональное выгорание",
       description:
         "Предназначен для измерения основных показателей синдрома профессионального выгорания",
     },
     {
+      id: "4",
       title: "Шкала тревоги",
       description:
         "Тест определяет уровень тревожности исходя из шкалы самооценки.",
     },
     {
+      id: "5",
       title: "Шкала депрессии, тревоги и стресса",
       description:
         "Является одной из наиболее распространенных современных шкал психологического дискомфорта.",
-    },
-  ];
-
-  const tasks: TaskInfo[] = [
-    {
-      title: "Трекер настроения",
-      description:
-        "Отмечай своё настроение каждый день, чтобы быть в курсе своего состояния и отслеживать его!",
-    },
-    {
-      title: "Дневник мыслей",
-      description:
-        "Расскажи, как прошёл твой день или что тебя тревожит. Дневник сохранит всё в тайне:)",
-    },
-  ];
-  const learning: TaskInfo[] = [
-    {
-      title: "Анализ убеждений",
-      description:
-        "Давай поработаем на твоими убеждениями и попробуем изменить их",
-    },
-    {
-      title: "Проверка убеждений",
-      description:
-        "Иногда необходимо рефлексировать и обдумывать то, что направляет тебя каждый день.",
-    },
-    {
-      title: "Анализ проблемы",
-      description:
-        "Ты обязательно справишься с любой своей проблемой, а мы поможем сделать это экологично!",
-    },
-    {
-      title: "P- и диалог",
-      description:
-        "У дизайнера кончились идеи на придумывание текста для заданий. Тут диалог:)",
     },
   ];
 
@@ -97,40 +52,14 @@ function GiveExercisesPage() {
       <div className={styles.section}>
         <Subtitle>Тесты</Subtitle>
         <div className={styles.list}>
-          {tests.map((test, index) => {
+          {tests.map((test) => {
             return (
               <ExerciseCard
-                key={index}
+                key={test.id}
+                id={test.id}
                 title={test.title}
                 description={test.description}
-              />
-            );
-          })}
-        </div>
-      </div>
-      <div className={styles.section}>
-        <Subtitle>Задания</Subtitle>
-        <div className={styles.list}>
-          {tasks.map((task, index) => {
-            return (
-              <ExerciseCard
-                key={index}
-                title={task.title}
-                description={task.description}
-              />
-            );
-          })}
-        </div>
-      </div>
-      <div className={styles.section}>
-        <Subtitle>Психообразование</Subtitle>
-        <div className={styles.list}>
-          {learning.map((learning, index) => {
-            return (
-              <ExerciseCard
-                key={index}
-                title={learning.title}
-                description={learning.description}
+                userId={searchParams.get("userId") ?? ""}
               />
             );
           })}
