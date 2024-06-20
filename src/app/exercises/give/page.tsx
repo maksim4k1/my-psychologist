ec348e5b8956e52dab1ee160356c07a9bb747477
maksim4k1 <maksim4k1@gmail.com>
2024-06-20 12:22:18 +0700

    added tests rendering on GiveExercise page

--- src/app/exercises/give/page.tsx
+++ src/app/exercises/give/page.tsx
@@ -9,67 +9,51 @@ import { ACCESS } from "../../../../config/access.config";
 import { TestData } from "@/redux/features/tests/types";
 import { useSearchParams } from "next/navigation";
 import ExerciseCard from "@/components/UI/Cards/ExerciseCard";
+import { useAppDispatch, useAppSelector } from "@/hooks/reduxHooks";
+import {
+  selectGetTestsState,
+  selectTests,
+} from "@/redux/features/tests/selectors";
+import { useEffect } from "react";
+import TestsService from "@/api/tests";
+import LoadingWrapper from "@/components/wrappers/LoadingWrapper";
 
-function GiveExercisesPage() {
+function GiveExercisePage() {
   const searchParams = useSearchParams();
+  const dispatch = useAppDispatch();
+  const tests: TestData[] = useAppSelector(selectTests);
+  const testsState = useAppSelector(selectGetTestsState);
 
-  const tests: TestData[] = [
-    {
-      id: "1",
-      title: "Тест на депрессию",
-      description:
-        "Тест по методике знаменитого американского психотерапевта Аарона Бека",
-    },
-    {
-      id: "2",
-      title: "Индикатор копинг-стратегий",
-      description:
-        "Предназначен для диагностики доминирующих копинг-стратегий личности",
-    },
-    {
-      id: "3",
-      title: "Профессиональное выгорание",
-      description:
-        "Предназначен для измерения основных показателей синдрома профессионального выгорания",
-    },
-    {
-      id: "4",
-      title: "Шкала тревоги",
-      description:
-        "Тест определяет уровень тревожности исходя из шкалы самооценки.",
-    },
-    {
-      id: "5",
-      title: "Шкала депрессии, тревоги и стресса",
-      description:
-        "Является одной из наиболее распространенных современных шкал психологического дискомфорта.",
-    },
-  ];
+  useEffect(() => {
+    dispatch(TestsService.getTests());
+  }, []);
 
   return (
     <Container>
       <PageTitle>Задания для клиента</PageTitle>
       <div className={styles.section}>
-        <Subtitle>Тесты</Subtitle>
-        <div className={styles.list}>
-          {tests.map((test) => {
-            return (
-              <ExerciseCard
-                key={test.id}
-                id={test.id}
-                title={test.title}
-                description={test.description}
-                userId={searchParams.get("userId") ?? ""}
-              />
-            );
-          })}
-        </div>
+        <LoadingWrapper status={testsState.isLoading}>
+          <Subtitle>Тесты</Subtitle>
+          <div className={styles.list}>
+            {tests.map((test) => {
+              return (
+                <ExerciseCard
+                  key={test.id}
+                  id={test.id}
+                  title={test.title}
+                  description={test.description}
+                  userId={searchParams.get("userId") ?? ""}
+                />
+              );
+            })}
+          </div>
+        </LoadingWrapper>
       </div>
     </Container>
   );
 }
 
-export default checkAuth(GiveExercisesPage, true, [
+export default checkAuth(GiveExercisePage, true, [
   ACCESS.psychologist,
   ACCESS.hr,
 ]);
