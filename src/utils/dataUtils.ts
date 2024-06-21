d1853f2dbafbaad4e237977e4bb444d63c00954a
maksim4k1 <maksim4k1@gmail.com>
2024-06-21 22:34:50 +0700

    created mapAgeToText util

--- src/utils/dataUtils.ts
+++ src/utils/dataUtils.ts
@@ -20,3 +20,21 @@ export const calculateAge = (date: string): number => {
 
   return age;
 };
+
+export const mapAgeToText = (age: number): string => {
+  const ageStr = String(age);
+  if (age < 1 || age % 1 !== 0) return ageStr;
+  const lastNumber: number = Number(ageStr.at(-1));
+
+  const l = "лет";
+  const g = "год";
+  const ga = "года";
+
+  if (age === 11 || age === 12 || age == 13 || age === 14)
+    return ageStr + " " + l;
+  else if (lastNumber === 1) return ageStr + " " + g;
+  else if (lastNumber === 2 || lastNumber === 3 || lastNumber === 4)
+    return ageStr + " " + ga;
+
+  return ageStr + " " + l;
+};
