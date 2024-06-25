48932be921dddfc41e89e69c52eda7b26ebac0a7
maksim4k1 <maksim4k1@gmail.com>
2024-06-25 20:04:41 +0700

    implemented test results to results page and edited mapToRadarChartData util

--- src/app/results/[id]/page.tsx
+++ src/app/results/[id]/page.tsx
@@ -8,69 +8,20 @@ import RadarChart from "@/components/UI/Charts/RadarChart";
 import ListItemWithSwitch from "@/components/UI/Lists/ListItemWithSwitch";
 import { useCheckbox } from "@/hooks/inputHooks";
 import { useEffect, useState } from "react";
-import {
-  DateData,
-  RadarChartItem,
-  mapToRadarChartData,
-} from "@/utils/chartUtils";
+import { RadarChartItem, mapToRadarChartData } from "@/utils/chartUtils";
 import { ACCESS } from "../../../../config/access.config";
 import { useParams, useSearchParams } from "next/navigation";
-import {
-  addQueryParams,
-  checkQueryParams,
-  mapSearchParamsToObject,
-} from "@/utils/urlUtils";
+import { checkQueryParams } from "@/utils/urlUtils";
 import HttpErrorWrapper from "@/components/wrappers/HttpErrorWrapper";
 import { useAppDispatch, useAppSelector } from "@/hooks/reduxHooks";
 import TestsService from "@/api/tests";
 import StateWrapper from "@/components/wrappers/StateWrapper";
-import { selectGetTestInfoState } from "@/redux/features/tests/selectors";
-
-const dates: DateData[] = [
-  {
-    date: "15 апреля",
-    values: [34, 20, 17],
-  },
-  {
-    date: "8 мая",
-    values: [15, 35, 25],
-  },
-  {
-    date: "18 марта",
-    values: [45, 32, 20],
-  },
-  {
-    date: "12 февраля",
-    values: [46, 28, 25],
-  },
-  {
-    date: "12 января",
-    values: [15, 15, 15],
-  },
-  {
-    date: "5 декабря 2023 года",
-    values: [24, 20, 23],
-  },
-  {
-    date: "2  ноября 2023 года",
-    values: [12, 2, 10],
-  },
-];
-
-const subjects: RadarChartItem[] = [
-  {
-    subject: "Эмоциональное истощение",
-    fullMark: 54,
-  },
-  {
-    subject: "Редукция проф. достижений",
-    fullMark: 40,
-  },
-  {
-    subject: "Деперсонализация",
-    fullMark: 30,
-  },
-];
+import {
+  selectGetTestInfoState,
+  selectGetTestResultsState,
+  selectTestInfo,
+  selectTestResults,
+} from "@/redux/features/tests/selectors";
 
 function PsychologistClientsOverallResultPage() {
   const { id } = useParams();
@@ -79,14 +30,26 @@ function PsychologistClientsOverallResultPage() {
   const [data, setData] = useState<RadarChartItem[]>([]);
   const dispatch = useAppDispatch();
   const getTestInfoState = useAppSelector(selectGetTestInfoState);
+  const getTestResultsState = useAppSelector(selectGetTestResultsState);
+  const testInfo = useAppSelector(selectTestInfo);
+  const testResults = useAppSelector(selectTestResults);
 
   useEffect(() => {
     dispatch(TestsService.getTestInfo(id));
+    dispatch(TestsService.getTestResults(id, searchParams.get("userId")));
   }, [dispatch, id]);
 
   useEffect(() => {
-    setData(mapToRadarChartData(dates, subjects, datesCheckboxes.value));
-  }, [datesCheckboxes.value]);
+    if (testResults && testInfo) {
+      setData(
+        mapToRadarChartData(
+          testResults,
+          testInfo.scales,
+          datesCheckboxes.value,
+        ),
+      );
+    }
+  }, [datesCheckboxes.value, testResults, testInfo]);
 
   return (
     <HttpErrorWrapper
@@ -94,29 +57,32 @@ function PsychologistClientsOverallResultPage() {
       error={{ status: 400, message: "" }}
     >
       <Container>
-        <StateWrapper state={getTestInfoState}>
-          <PageTitle className={styles.title}>История теста: Маслач</PageTitle>
-          <RadarChart
-            data={data}
-            className={styles.radarChart}
-          />
-          <ul>
-            {dates.map((el) => {
-              return (
-                <ListItemWithSwitch
-                  onChange={datesCheckboxes.onChange}
-                  className={styles.listItem}
-                  value={el.date}
-                  key={el.date}
-                  label={el.date}
-                  link={addQueryParams(`/results/detail/${id}`, {
-                    date: el.date,
-                    ...mapSearchParamsToObject(searchParams),
-                  })}
-                />
-              );
-            })}
-          </ul>
+        <StateWrapper state={[getTestInfoState, getTestResultsState]}>
+          {testInfo && testResults && (
+            <>
+              <PageTitle className={styles.title}>
+                История теста: {testInfo.title}
+              </PageTitle>
+              <RadarChart
+                data={data}
+                className={styles.radarChart}
+              />
+              <ul>
+                {testResults.map((el) => {
+                  return (
+                    <ListItemWithSwitch
+                      onChange={datesCheckboxes.onChange}
+                      className={styles.listItem}
+                      value={el.id}
+                      key={el.id}
+                      label={el.datetime}
+                      link={`/results/detail/${el.id}`}
+                    />
+                  );
+                })}
+              </ul>
+            </>
+          )}
         </StateWrapper>
       </Container>
     </HttpErrorWrapper>
