610dd0773c1579df6259b9f9c8cda3ab18ff64a1
maksim4k1 <maksim4k1@gmail.com>
2024-08-08 03:49:30 +0700

    добавил всю логику по прохождению теста, кроме отправки результатов

--- src/app/(app)/exercises/[id]/page.tsx
+++ src/app/(app)/exercises/[id]/page.tsx
@@ -9,31 +9,93 @@ import { ACCESS } from "@/config/access.config";
 import { useAppDispatch, useAppSelector } from "@/hooks/reduxHooks";
 import {
   selectGetTestInfoState,
+  selectGetTestQuestionsState,
   selectTestInfo,
+  selectTestQuestions,
 } from "@/redux/features/tests/selectors";
 import { useParams } from "next/navigation";
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 import styles from "./styles.module.scss";
-import PrimaryButton from "@/components/UI/Buttons/PrimaryButton";
+import Radio from "@/components/UI/Inputs/Radio";
+import Button from "@/components/UI/Buttons/Button";
+import ArrowIcon from "@/assets/svg/Icons/ArrowIcon";
+import { useSetDefaultState } from "@/hooks/setDefaultStateHook";
+import { testsActions } from "@/redux/features/tests";
 
 const ExercisePage: FC = () => {
   const { id } = useParams<{ id: string }>();
   const dispatch = useAppDispatch();
   const testInfo = useAppSelector(selectTestInfo);
   const getTestInfoState = useAppSelector(selectGetTestInfoState);
+  const testQuestions = useAppSelector(selectTestQuestions);
+  const getTestQuestionsState = useAppSelector(selectGetTestQuestionsState);
+  const [currentQuestionNumber, setCurrentQuestion] = useState(0);
+  const [results, setResults] = useState<number[]>([]);
 
   useEffect(() => {
     dispatch(TestsService.getTestInfo(id));
+    dispatch(TestsService.getTestQuestions(id));
   }, [dispatch, id]);
+
+  const selectAnswerHandler = (number: number, score: number) => {
+    const newResultsValue = [...results];
+    newResultsValue[number - 1] = score;
+    setResults(newResultsValue);
+  };
+
+  const showQuestion = (index: number) => {
+    setCurrentQuestion(index);
+  };
+
+  useSetDefaultState(testsActions.getTestInfoSetDefaultState);
+  useSetDefaultState(testsActions.getTestQuestionsSetDefaultState);
+
+  const currentQuestion = testQuestions && testQuestions[currentQuestionNumber];
 
   return (
     <Container>
-      <StateWrapper state={getTestInfoState}>
-        {testInfo && (
+      <StateWrapper state={[getTestInfoState, getTestQuestionsState]}>
+        {testInfo && testQuestions && currentQuestion && (
           <>
             <PageTitle className={styles.title}>{testInfo.title}</PageTitle>
-            <p className={styles.description}>{testInfo.description}</p>
-            <PrimaryButton>Начать тест</PrimaryButton>
+            <div className={styles.header}>
+              <h4 className={styles.question}>{currentQuestion.title}</h4>
+              <div className={styles.questionNumber}>
+                {currentQuestionNumber + 1}/{testQuestions.length}
+              </div>
+            </div>
+            <div className={styles.radioButtons}>
+              {currentQuestion.answers.map((answer) => {
+                return (
+                  <Radio
+                    key={answer.id}
+                    name="question"
+                    labelText={answer.text}
+                    onClick={() =>
+                      selectAnswerHandler(currentQuestion.number, answer.score)
+                    }
+                  />
+                );
+              })}
+            </div>
+            <div className={styles.buttons}>
+              <Button
+                className={styles.button}
+                onClick={() => showQuestion(currentQuestionNumber - 1)}
+                disabled={currentQuestionNumber <= 0}
+              >
+                <ArrowIcon className={styles.backIcon} />
+                Предыдущий вопрос
+              </Button>
+              <Button
+                className={styles.button}
+                onClick={() => showQuestion(currentQuestionNumber + 1)}
+                disabled={currentQuestionNumber + 1 >= testQuestions.length}
+              >
+                Следующий вопрос
+                <ArrowIcon />
+              </Button>
+            </div>
           </>
         )}
       </StateWrapper>
