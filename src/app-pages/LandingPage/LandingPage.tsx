cd665544f1a2a5b115b30a7997eac5c84a64145c
maksim4k1 <maksim4k1@gmail.com>
2024-10-26 22:12:20 +0700

    #15 переместил Header и Footer лендинга в папку LandingPage/components

--- src/app-pages/LandingPage/LandingPage.tsx
+++ src/app-pages/LandingPage/LandingPage.tsx
@@ -1,19 +1,33 @@
-import { Achievements, Advantages, Hero, Partners, Team } from "./components";
+import {
+  Achievements,
+  Advantages,
+  Footer,
+  Header,
+  Hero,
+  Partners,
+  Team,
+} from "./components";
 import styles from "./styles.module.scss";
 import Container from "@/components/UI/Container";
 import { type FC } from "react";
 
 export const LandingPage: FC = () => {
   return (
-    <Container
-      isLarge={true}
-      className={styles.container}
-    >
-      <Hero />
-      <Advantages />
-      <Partners />
-      <Achievements />
-      <Team />
-    </Container>
+    <>
+      <Header />
+      <main className={styles.main}>
+        <Container
+          isLarge={true}
+          className={styles.container}
+        >
+          <Hero />
+          <Advantages />
+          <Partners />
+          <Achievements />
+          <Team />
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 };
