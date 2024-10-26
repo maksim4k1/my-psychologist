8fcf777d0cdd72d9a4519cc5c973567956509b66
maksim4k1 <maksim4k1@gmail.com>
2024-10-27 01:13:45 +0700

    #15 вынес картинку на главном экране в папку public

--- src/client/pages/Landing/components/Hero/index.tsx
+++ src/client/pages/Landing/components/Hero/index.tsx
@@ -1,6 +1,4 @@
 import styles from "./styles.module.scss";
-import Image from "next/image";
-import MainImage from "@/client/assets/webp/meditaion.webp";
 import { type FC } from "react";
 
 export const Hero: FC = () => {
@@ -12,13 +10,6 @@ export const Hero: FC = () => {
         <strong className={styles.strong}>эмоциональной стабильности</strong> с
         нами!
       </h1>
-      <Image
-        width={MainImage.width}
-        height={MainImage.height}
-        src={MainImage.src}
-        alt="Найди путь к душевному спокойствию"
-        className={styles.mainSectionImage}
-      />
     </section>
   );
 };
