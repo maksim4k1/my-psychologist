dc1c78acd7090ad1d2beb9e8e030f4b9c397da23
maksim4k1 <maksim4k1@gmail.com>
2024-11-16 16:15:23 +0700

    #26 подготовил страницу с отзывами от пользователей

--- src/client/pages/Admin/UsersFeedbacks/UsersFeedbacks.tsx
+++ src/client/pages/Admin/UsersFeedbacks/UsersFeedbacks.tsx
@@ -1,5 +1,51 @@
+"use client";
+
+import {
+  useDeleteFeedbackMutation,
+  useGetFeedbacksQuery,
+  useMarkFeedbackAsReadedMutation,
+} from "@/client/redux";
 import { type FC } from "react";
 
 export const UsersFeedbacksPage: FC = () => {
-  return <div>feedbacks</div>;
+  const { data: feedbacks, ...getFeedbacksStatus } = useGetFeedbacksQuery();
+  const [markFeedbackAsReaded, markFeedbackAsReadedStatus] =
+    useMarkFeedbackAsReadedMutation();
+  const [deleteFeedback, deleteFeedbackStatus] = useDeleteFeedbackMutation();
+
+  console.log(
+    feedbacks,
+    getFeedbacksStatus,
+    markFeedbackAsReadedStatus,
+    deleteFeedbackStatus,
+  );
+
+  const markAsReadedHandler = (id: string) => {
+    markFeedbackAsReaded(id);
+  };
+
+  const deleteHandler = (id: string) => {
+    deleteFeedback(id);
+  };
+
+  return (
+    <div>
+      feedbacks:
+      {feedbacks &&
+        feedbacks.map(({ id, email, feedback, isReaded }) => (
+          <div key={id}>
+            <h3>{email}</h3>
+            <h3>{feedback}</h3>
+            <h3>isReaded: {isReaded ? "true" : "false"}</h3>
+            <button onClick={() => markAsReadedHandler(id)}>
+              mark as readed
+            </button>
+            <br />
+            <button onClick={() => deleteHandler(id)}>delete</button>
+            <br />
+            <br />
+          </div>
+        ))}
+    </div>
+  );
 };
