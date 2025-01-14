2fa41d357de54af9926cb5474a211ec473bebd68
maksim4k1 <maksim4k1@gmail.com>
2025-01-14 20:40:27 +0700

    FE-32 починили отображение теории

--- src/server/routes/articles/article.ts
+++ src/server/routes/articles/article.ts
@@ -5,36 +5,15 @@ import { httpStatuses } from "@/shared/data";
 import {
   type GetArticleApiResponseData,
   type GetArticleResponseData,
-  type GetArticlesApiResponseData,
-  ResponseError,
 } from "@/shared/types";
 
 const getArticle = createRequest<{ id: string }>(
   async (request, serverFetch, { id }) => {
-    const articlePromise = serverFetch.get<GetArticleApiResponseData>(
+    const articleResponse = await serverFetch.get<GetArticleApiResponseData>(
       `/education/get_all_education_material/${id}`,
     );
 
-    const articlesPromise = serverFetch.get<GetArticlesApiResponseData>(
-      "/education/get_all_theme",
-    );
-
-    const [articleResponse, articlesResponse] = await Promise.all([
-      articlePromise,
-      articlesPromise,
-    ]);
-
-    const currentArticle = articlesResponse.data.find((el) => el.id === id);
-
-    if (!currentArticle) {
-      return NextResponse.json(
-        new ResponseError(httpStatuses.notFound.status, "Не найдено"),
-        httpStatuses.notFound,
-      );
-    }
-
     const data: GetArticleResponseData = mapGetArticleResponse(
-      currentArticle,
       articleResponse.data,
     );
 
